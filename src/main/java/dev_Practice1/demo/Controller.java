1998281b45781a1adf1f978db2390bd5241c3724
sid <siddharthingle1010@gmail.com>
2023-10-09 19:10:59 +0530

    commit1

---
+++ src/main/java/dev_Practice1/demo/Controller.java
@@ -0,0 +1,14 @@
+package dev_Practice1.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/greet")
+    public String greet(){
+        return "Hi Siddhu";
+    }
+}
+
